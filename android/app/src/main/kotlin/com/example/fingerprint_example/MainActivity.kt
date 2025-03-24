fee3eedba29ada9a41ba77d41fcb3ed25268de11
Nada Nabil <nadanabilhelmyy@gmail.com>
2025-03-24 21:43:05 +0200

    fixed biometric prompt error

--- android/app/src/main/kotlin/com/example/fingerprint_example/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/fingerprint_example/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.fingerprint_example
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity() {
+}
